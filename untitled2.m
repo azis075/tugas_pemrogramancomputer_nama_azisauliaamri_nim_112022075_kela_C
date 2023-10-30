21b9564bd5978d87c799bf2c3f3c485c9a80a4b0
azis075 <149403675+azis075@users.noreply.github.com>
2023-10-30 22:21:55 +0700

    Add files via upload

---
+++ untitled2.m
@@ -0,0 +1,23 @@
+
+clc
+clear
+%Soal Matrik 4.2 (Soal 2)
+%Diket Matrik 
+
+%Dengan Menggunakan Matlab, Buatlah :
+
+%Soal A. Matrik Indentitas Dengan Dimensi 5x5
+A= eye(5,5)
+
+%Soal B. Matrik Dengan Dimensi 3x3 Dengan Elemen Semuanya Bernilai Nol
+B= zeros(3,3)
+
+%Soal C. Matrik Dengan Dimensi 4x4 Dengan Elemen Semuanya Bernilai Satu
+C= ones(4,4)
+
+%Soal D. Matrik Dengan Dimensi 3x4 Dimana Elemen Terdistribusi Antara 1 dan 0 
+%(default n=m)
+D= rand(3,4) 
+
+%Soal E. Matrik Dengan Dimensi 3x4 Dimana Elemen Terdistribusi Normal 
+E= randn (3,4)
